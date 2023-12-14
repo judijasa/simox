c9fd4292b51aaca7a0a8b4a1b7f3cd190fb999de
judijasa <judijasa@gmail.com>
2023-12-14 14:55:58 -0500

    Create file to test connectivity with database

---
+++ private/test_connection.php
@@ -0,0 +1,36 @@
+<?php
+    // File to test the PHP extension: PDO MYSQL AND mysqli
+    // https://www.php.net/manual/en/ref.pdo-mysql.php
+
+    // PHP Connect to MySQL
+    // https://www.w3schools.com/php/php_mysql_connect.asp
+
+    $cnf = parse_ini_file("client_config.sh"); // INI format similar to SH
+    $servername = $cnf["SERVER"];
+    $username = $cnf["USER"];
+    $password = $cnf["PASSWORD"];
+    $dbname = $cnf["DATABASE"];
+
+    // MySQLi Procedural
+    // create connection
+    /*
+    $conn = mysqli_connect($servername, $username, $password);
+
+    // check connection
+    if (! $conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    echo "Connected successfully";
+    }
+     */
+
+    // Highly compatible alternative...
+    // PDO
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
