bf5bce4dfffa38146fa7815a1431e4d59d040e46
judijasa <judijasa@gmail.com>
2024-03-06 19:03:46 -0500

    Add new public file: 'Analisis de datos'. public/insight.php

---
+++ public/insight.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<!--
+Content: Display SQL Tables with pagination
+Source: www.javatpoint.com/php-pagination
+
+Browser address:
+http://localhost/web-projects/scraping_SIMO/index.php
+
+Author: judijasa <ciudadania.ab@gmail.com>
+-->
+
+    <head>
+        <title>SimoEx:Insight</title>
+        <meta name="viewport" charset="utf-8" content="width=device-width, initial-scale=1">
+
+        <!-- More: http://www.webweaver.nu/html-tips/favicon.shtml -->
+        <link rel="shortcut icon" href="favicon.ico">
+
+        <!-- My custom CSS-->
+        <!-- Uncommented in original config
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+        -->
+
+        <!-- Bootstrap 3 HMTL Framework (plugin) -->
+        <!--
+        <link rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        -->
+
+        <!-- Bootstrap 5 HTML Framework (plugin)
+             https://getbootstrap.com/docs/5.1/getting-started/introduction/
+        -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
+
+        <!-- Load search icon library
+        www.w3schools.com/howto/howto_css_search_button.asp
+        nothing here
+        -->
+
+        <!-- Load arrow icon script src
+        www.w3schools.com/icons/tryit.asp?icon=fas_fa-angle-left&unicon=f104
+        -->
+        <script src='https://kit.fontawesome.com/1d6d59d2e9.js' crossorigin='anonymous'></script>
+
+        <!-- Twitter Bootstrap: Button to match the style of the select menu with selectBoxIt
+
+        www.c-sharpcorner.com/UploadFile/736ca4/twitter-bootstrap-3-layout-and-buttons/
+        -->
+
+        <!-- Bootstrap HTML Framework (from local file) -->
+        <!-- Uncommentd in original config
+        <link href="bootstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="bootstrap/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" />
+        <script src="bootstrap/bootstrap/js/bootstrap.min.js"></script>
+        -->
+
+        <!--
+             To handle long text in select options
+             Required links:
+             gregfranko.com/jquery.selectBoxIt.js/#GettingStarted
+             Theme: SelectBoxIt with Twitter Bootstrap
+        -->
+            <link type="text/css" rel="stylesheet" href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" />
+            <link type="text/css" rel="stylesheet" href="http://gregfranko.com/jquery.selectBoxIt.js/css/jquery.selectBoxIt.css" />
+    </head>
+    <body>
+        <div class="container">
+            <center>
+            <h2>Análisis de los datos reportados</h2>
+            <p>[UNDER CONSTRUCTION]</p>
+        </div>
+    </body>
+</html>
