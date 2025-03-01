64389fc7fb96505be6f93389c4e8cb119ec0d1ea
Juan Diego Jaramillo <judijasa@gmail.com>
2025-03-01 22:59:24 +0000

    Update deploy.sh to include cron jobs.

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,40 +1,69 @@
 #!/bin/bash
 
 # If exec not using CRON, check if you're in repo's root dir
-root_dir=$(git rev-parse --show-toplevel) # repo root directory path
-if [[ "$PWD" != $root_dir ]]
+# Using SIMO_REPO_PATH instead of root_dir because git rev-parse only works within repo
+# root_dir=$(git rev-parse --show-toplevel) # repo root directory path
+if [[ "$PWD" != "$SIMO_REPO_PATH" ]]
 then
   echo "This command must be executed from the repository's root directory."
   exit
 fi
 
-# Define variables
-SOURCE_DIR=$root_dir
-DEST_DIR="/var/www/html/simo-express"
-FILES_TO_DEPLOY=()
+deploy_website() {
+  # Define variables
+  local SOURCE_DIR=$SIMO_REPO_PATH
+  local DEST_DIR="/var/www/html/simo-express"
+  local FILES_TO_DEPLOY=()
 
-# Ensure source directory exists
-if [ -d "$SOURCE_DIR/public" ]; then
-    # Populate FILES_TO_DEPLOY with all files in the public directory
-    mapfile -t FILES_TO_DEPLOY < <(find "$SOURCE_DIR/public" -type f)
-else
-    echo "Error: Source directory 'public' not found."
-    exit 1
-fi
+  # Ensure source directory exists
+  if [ -d "$SOURCE_DIR/public" ]; then
+      # Populate FILES_TO_DEPLOY with all files in the public directory
+      mapfile -t FILES_TO_DEPLOY < <(find "$SOURCE_DIR/public" -type f)
+  else
+      echo "Error: Source directory 'public' not found."
+      exit 1
+  fi
 
-# Ensure destination directory exists
-mkdir -p "$DEST_DIR"
+  # Ensure destination directory exists
+  mkdir -p "$DEST_DIR"
 
-# Copy files to destination directory
-for file in "${FILES_TO_DEPLOY[@]}"; do
-    cp "$file" "$DEST_DIR"
-done
+  # Copy files to destination directory
+  for file in "${FILES_TO_DEPLOY[@]}"; do
+      cp "$file" "$DEST_DIR"
+  done
 
-# Optionally, you can also restart your web server to apply changes
-# For Apache:
-systemctl restart apache2
+  # Optionally, you can also restart your web server to apply changes
+  # For Apache:
+  systemctl restart apache2
 
-# For Nginx:
-# systemctl restart nginx
+  # For Nginx:
+  # systemctl restart nginx
+}
+
+deploy_cronjobs() {
+  local MAINTENANCE="src/scripts/maintenance"
+  local INDEXER="src/scripts/simo_indexer"
+
+  cron_cp() {
+    local SOURCE="$1"
+    local TARGET="$2"
+    cp "$SOURCE" "$TARGET"
+    chmod +x $TARGET
+  }
+  # run-parts (cron way to exec all files in a directory) adhere to a specific naming convention.
+  # Avoid files with extensions; use myfile instead of myfile.sh. It also must be executable,
+  # hence chmod + x myfile
+  # You need to create /etc/cron.d/cron-5min and add the line
+  # */5 * * * * root run-parts /etc/cron.5min
+  mkdir -p /etc/cron.5min
+  cron_cp "$MAINTENANCE/memory_cleaning.sh" "/etc/cron.5min/simo_memory_cleaning"
+  cron_cp "$MAINTENANCE/trim_log_files.sh" "/etc/cron.monthly/simo_trim_log_files"
+  cron_cp "$INDEXER/main.sh" "/etc/cron.monthly/simo_main"
+
+  sudo systemctl restart cron
+}
+
+deploy_website()
+deploy_cronjobs()
 
 # Optionally, you can also clear any caches or perform other post-deployment tasks
