c20ac7414aff4ba62b8cb5b326dcae39b2060499
Juan Diego Jaramillo <judijasa@gmail.com>
2025-03-01 20:20:39 +0000

    Add RAM maintenance script.

---
+++ src/scripts/maintenance/memory_cleaning.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This is meant to be executed every 5min
+# Kill memory intensive processes if exceed threshold (> 90%)
+# If above threshold but no process found, reboot.
+
+repoPath="/home/admin/git/SIMOExpress"
+logFile="$repoPath/log/maintenance.log"
+
+THRESHOLD=90
+USED_RAM=$(free | awk '/Mem:/ {printf "%.0f", $3/$2 * 100}')
+
+if [ ! -f "$logFile" ]; then
+  touch "$logFile"
+fi
+
+if [ "$USED_RAM" -ge "$THRESHOLD" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - RAM usage at ${USED_RAM}%, exceeding threshold. Finding and killing top memory consumer..." | tee -a $logFile
+    TOP_PID=$(ps -eo pid,%mem,cmd --sort=-%mem | awk 'NR==2 {print $1}')
+    if [ -n "$TOP_PID" ]; then
+        echo "Killing process $TOP_PID"
+        kill -9 "$TOP_PID"
+    else
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - No process found, rebooting as last resort..." | tee -a $logFile
+        sudo reboot
+    fi
+fi
