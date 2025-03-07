2dfe3f9a223f4dae8ec29e97ca869c7bcab92a42
Juan Diego Jaramillo <judijasa@gmail.com>
2025-03-07 02:49:26 +0000

    Update atelier/test_raw_connections_with_PDO.php

--- atelier/test_raw_connections_with_PDO.php
+++ atelier/test_raw_connections_with_PDO.php
@@ -5,31 +5,31 @@
     // PHP Connect to MySQL
     // https://www.w3schools.com/php/php_mysql_connect.asp
 
-    $cnf = parse_ini_file("private/client_config.sh"); // INI format similar to SH
+    $cnf = parse_ini_file("src/config.sh"); // INI format similar to SH
     $servername = $cnf["SERVER"];
-    $username = $cnf["USER"];
-    $password = $cnf["PASSWORD"];
-    $dbname = $cnf["DATABASE"];
+    $username = 'public';
+    $password = '';
+    $dbname = $cnf["DBNAME"];
 
-    // MySQLi Procedural
-    // create connection
-    /*
+    // MySQLi driver (using its procedure style instead of its object oriented style)
+
     $conn = mysqli_connect($servername, $username, $password);
 
     // check connection
-    if (! $conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    echo "Connected successfully";
+    if (mysqli_connect_errno()) {
+      die("Connection failed: " . mysqli_connect_error());
+    } else {
+      echo "Connected successfully via mysqli_connect.". PHP_EOL;
+      mysqli_close($conn);
     }
-     */
 
-    // Highly compatible alternative...
-    // PDO
+    // Highly compatible alternative... (recommended)
+    // MySQL PDO driver (using its object oriented style)
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+        echo "Connected successfully via MySQL PDO.". PHP_EOL;
         $conn = null;
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
