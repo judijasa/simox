6a708982f58075c2826e2ed87d6eb51a1ea8d50c
Juan Diego Jaramillo <judijasa@gmail.com>
2025-03-26 14:20:39 +0000

    Add column 'iso' to dpto_colombia tbl

--- pkg/dpto_colombia-BD672733D7AA455F/upgrade.sql
+++ pkg/dpto_colombia-BD672733D7AA455F/upgrade.sql
@@ -3,28 +3,30 @@ DROP TABLE IF EXISTS job_offer;
 CREATE OR REPLACE TABLE dpto_colombia (
     id TINYINT AUTO_INCREMENT,
     nombre VARCHAR(75) UNIQUE NOT NULL,
+    nombre VARCHAR(5) UNIQUE NOT NULL,
 
     PRIMARY KEY(id)
 );
 
 INSERT INTO dpto_colombia (
-    nombre
+    nombre,
+    iso
 ) VALUES
-    ('Amazonas'), ('Antioquia'),
-    ('Arauca'), ('Archipiélago de San Andrés, Providencia y Santa Catalina'),
-    ('Atlántico'), ('Bogotá D.C.'),
-    ('Bolívar'), ('Boyacá'), ('Caldas'),
-    ('Caquetá'), ('Casanare'),
-    ('Cauca'), ('Cesar'),
-    ('Chocó'), ('Córdoba'),
-    ('Cundinamarca'), ('Guainía'),
-    ('Guaviare'), ('Huila'),
-    ('La Guajira'), ('Magdalena'),
-    ('Meta'), ('Nariño'),
-    ('Norte de Santander'), ('Putumayo'),
-    ('Quindío'), ('Risaralda'),
-    ('Santander'), ('Sucre'),
-    ('Tolima'), ('Valle del Cauca'),
-    ('Vaupés'), ('Vichada');
+    ('Amazonas', 'AMA'), ('Antioquia', 'ANT'),
+    ('Arauca', 'ARA'), ('Archipiélago de San Andrés, Providencia y Santa Catalina', 'SAP'),
+    ('Atlántico', 'ATL'), ('Bogotá', 'DC'),
+    ('Bolívar', 'BOL'), ('Boyacá', 'BOY'), ('Caldas', 'CAL'),
+    ('Caquetá', 'CAQ'), ('Casanare', 'CAS'),
+    ('Cauca', 'CAU'), ('Cesar', 'CES'),
+    ('Chocó', 'CHO'), ('Córdoba', 'COR'),
+    ('Cundinamarca', 'CUN'), ('Guainía', 'GUA'),
+    ('Guaviare', 'GUV'), ('Huila', 'HUI'),
+    ('La Guajira', 'LAG'), ('Magdalena', 'MAG'),
+    ('Meta', 'MET'), ('Nariño', 'NAR'),
+    ('Norte de Santander', 'NSA'), ('Putumayo', 'PUT'),
+    ('Quindío', 'QUI'), ('Risaralda', 'RIS'),
+    ('Santander', 'SAN'), ('Sucre', 'SUC'),
+    ('Tolima', 'TOL'), ('Valle del Cauca', 'VAC'),
+    ('Vaupés', 'VAU'), ('Vichada', 'VID');
 
 GRANT SELECT ON {{dbname}}.dpto_colombia TO 'public'@'{{servername}}';
