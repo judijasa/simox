0a036a6a332130c6d9ab9bc27b09b031ed073963
Juan Diego Jaramillo <judijasa@gmail.com>
2025-05-16 00:04:53 +0000

    Refine get_cursor utility

--- src/utils/db_ops.php
+++ src/utils/db_ops.php
@@ -9,7 +9,7 @@ function set_cursor($conn, $key, $value, $mod=0, $div=1) {
     $stmt->execute();
 }
 
-function get_cursor($conn, $key, $mod=0, $div=1) {
+function get_cursor($conn, $key, $mod=0, $div=1, $init_cursor=null) {
     $stmt = $conn->prepare("SELECT value FROM cursorseq WHERE `key` = :key AND `mod` = :mod AND `div` = :div");
     $stmt->bindValue(':key', $key);
     $stmt->bindValue(':mod', $mod);
@@ -19,8 +19,11 @@ function get_cursor($conn, $key, $mod=0, $div=1) {
     if($value !== false){
         return $value;
     }else{
-        set_cursor($conn, $key, $mod, $div);
-        return; // null
+        if($init_cursor !== null){
+            set_cursor($conn, $key, $init_val, $mod, $div);
+            return $init_cursor; // null
+        }
+        return false; // null
     }
 }
 ?>
