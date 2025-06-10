f3991a3c9db185198a1484b4eed4348b3056a389
Juan Diego Jaramillo <judijasa@gmail.com>
2025-06-10 00:21:37 +0000

    Change database to adapt better to api crawling. Remove some packages.

--- pkg/municipio-WK095781S7NA146R/upgrade.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS municipio;
-CREATE TABLE municipio (
-    id INT AUTO_INCREMENT,
-    nombre VARCHAR(75) NOT NULL,
-
-    PRIMARY KEY (id)
-);
-
-INSERT INTO municipio(id, nombre) VALUES (0, 'NONE'), (-1, 'No_Aplica');
