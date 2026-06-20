ba3dc3d8df1f8043534b4b7c615af2902b242a29
Juan Jaramillo <judijasa@gmail.com>
2026-06-19 21:51:39 -0500

    Use new Connectivity/Database class in atelier/test_connections_with_PDO.php

--- atelier/test_connections_with_PDO.php
+++ atelier/test_connections_with_PDO.php
@@ -1,46 +1,46 @@
 <?php
-// Test PDO children classes
+require 'vendor/autoload.php';
 
-    require 'src/utils/connectivity.php';
+use Utils\Connectivity\Database;
 
-    try {
-        $dbname = 'simo';
-        $conn = new adminPDO($dbname);
-        echo "Connected successfully with adminPDO". PHP_EOL;
-    } catch(PDOException $e) {
-        echo "Connection failed: ". $e->getMessage(). PHP_EOL;
-    } finally {
-        $conn = null;
-    }
+try {
+    $dbname = 'simo';
+    $conn = Database::admin($dbname);
+    echo "Connected successfully with Database::admin". PHP_EOL;
+} catch(PDOException $e) {
+    echo "Connection failed: ". $e->getMessage(). PHP_EOL;
+} finally {
+    $conn = null;
+}
 
 
-    try {
-        $dbname = 'simo';
-        $conn = new readerPDO($dbname);
-        echo "Connected successfully with readerPDO". PHP_EOL;
-    } catch(PDOException $e) {
-        echo "Connection failed: ". $e->getMessage(). PHP_EOL;
-    } finally {
-        $conn = null;
-    }
+try {
+    $dbname = 'simo';
+    $conn = Database::reader($dbname);
+    echo "Connected successfully with Database::reader". PHP_EOL;
+} catch(PDOException $e) {
+    echo "Connection failed: ". $e->getMessage(). PHP_EOL;
+} finally {
+    $conn = null;
+}
 
-    try {
-        $dbname = 'simo';
-        $conn = new publicPDO($dbname);
-        echo "Connected successfully with publicPDO". PHP_EOL;
-    } catch(PDOException $e) {
-        echo "Connection failed: ". $e->getMessage(). PHP_EOL;
-    } finally {
-        $conn = null;
-    }
+try {
+    $dbname = 'simo';
+    $conn = Database::public($dbname);
+    echo "Connected successfully with Database::public". PHP_EOL;
+} catch(PDOException $e) {
+    echo "Connection failed: ". $e->getMessage(). PHP_EOL;
+} finally {
+    $conn = null;
+}
 
-    try {
-        $dbname = 'simo';
-        $conn = publicMySQLi($dbname);
-        echo "Connected successfully with publicMySQLi". PHP_EOL;
-    } catch(PDOException $e) {
-        echo "Connection failed: ". $e->getMessage(). PHP_EOL;
-    } finally {
-        $conn = null;
-    }
-?>
+# try {
+    # $dbname = 'simo';
+    # $conn = publicMySQLi($dbname);
+    # echo "Connected successfully with publicMySQLi". PHP_EOL;
+# } catch(PDOException $e) {
+    # echo "Connection failed: ". $e->getMessage(). PHP_EOL;
+# } finally {
+#     $conn = null;
+# }
+
