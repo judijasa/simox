8de8186f74e3b0733f8785cc5d67d784e69d2c32
Juan Jaramillo <judijasa@gmail.com>
2026-07-09 11:44:19 -0500

    Fix deploy_nix_packages. src/deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -155,8 +155,7 @@ deploy_nix_packages() {
   local REMOTE_STORE_PATH
   REMOTE_STORE_PATH=$(readlink -f ./result)
   ssh "$PROD_USER@$REMOTE_HOST" "
-    set -e
-    ~/.nix-profile/bin/nix-store --add-root /usr/local/simox/result --realise \"$REMOTE_STORE_PATH\"
+    /home/$PROD_USER/.nix-profile/bin/nix-store --add-root /usr/local/simox/result --realise \"$REMOTE_STORE_PATH\"
   "
   rm -f result
 }
