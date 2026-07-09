80ca5b989d1816c119a5adfb3f9ef25b544d5c4c
Juan Jaramillo <judijasa@gmail.com>
2026-07-09 18:20:15 -0500

    Enable nix-command and flakes for remote build

--- src/deploy.sh
+++ src/deploy.sh
@@ -170,7 +170,13 @@ deploy_nix_packages() {
     rm -f result
   else
     echo "Building packages on remote (local=$LOCAL_ARCH, remote=$REMOTE_ARCH)..."
-    REMOTE_STORE_PATH=$(ssh "$PROD_USER@$REMOTE_HOST" "bash -l -c 'cd \"$REMOTE_TARGET_DIR\" && nix build >&2 && readlink -f result && rm -f result'")
+    REMOTE_STORE_PATH=$(ssh "$PROD_USER@$REMOTE_HOST" "
+        . /home/$PROD_USER/.nix-profile/etc/profile.d/nix.sh
+        cd '$REMOTE_TARGET_DIR'
+        nix --extra-experimental-features 'nix-command flakes' build >&2
+        readlink -f result
+        rm -f result
+    ")
   fi
 
   echo "Registering nix store root on remote..."
