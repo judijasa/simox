fa21a88ba480a34db1e31e89311e843fea9638e2
Juan Jaramillo <judijasa@gmail.com>
2026-07-14 09:58:30 -0500

    Create pkg/documento-B4389BGTHWELGSQP

---
+++ pkg/documento-B4389BGTHWELGSQP/upgrade.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE TABLE documento (
+    id INT AUTO_INCREMENT,
+    codigo INT,  -- simo id
+    ruta_archivo VARCHAR(100),
+    content_type VARCHAR(100),
+    version VARCHAR(100),
+    stage_id VARCHAR(100),
+    fecha VARCHAR(100), -- DATETIME
+    documento_origen_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY pk_documento_id(id),
+    UNIQUE INDEX uk_documento_codigo(codigo)
+);
+
+GRANT SELECT ON {{dbname}}.job_offer TO 'public'@'{{servername}}';
