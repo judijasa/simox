d3b41494ce8849d28d74ccfc969584c36a454eab
Juan Jaramillo <judijasa@gmail.com>
2026-07-14 15:18:07 -0500

    Remove municipio.departamento_id column. Add index ix_municipio_departamento.

--- pkg/municipio-WK095781S7NA146R/upgrade.sql
+++ pkg/municipio-WK095781S7NA146R/upgrade.sql
@@ -3,12 +3,10 @@ CREATE OR REPLACE TABLE municipio (
     code SMALLINT UNSIGNED NOT NULL, -- id at simo
     nombre VARCHAR(100) NOT NULL,
     departamento VARCHAR(100),
-    departamento_id TINYINT,
 
     PRIMARY KEY pk_municipio_id(id),
     UNIQUE KEY uk_municipio_code(code),
-    FOREIGN KEY fk_municipio_departamento_id(departamento_id)
-        REFERENCES departamento(id)
+    INDEX ix_municipio_departamento(departamento)
 );
 
 -- INSERT INTO municipio(id, code, nombre, departamento, departamento_id) VALUES (0, 0, 'No_Aplica', 'No_aplica', 34);
