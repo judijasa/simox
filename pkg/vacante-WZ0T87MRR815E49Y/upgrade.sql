a40d2690cd985f75d6551a273d7a1065a65a0015
Juan Jaramillo <judijasa@gmail.com>
2026-07-14 16:14:26 -0500

    Remove comment. vacante-WZ0T87MRR815E49Y/upgrade.sql

--- pkg/vacante-WZ0T87MRR815E49Y/upgrade.sql
+++ pkg/vacante-WZ0T87MRR815E49Y/upgrade.sql
@@ -1,8 +1,3 @@
-/*
-To DROP a parent tbl, first you need to DROP its child tbls,
-that is why we make sure job_offer is dropped before
-replacing the vacante table.
-*/
 CREATE OR REPLACE TABLE vacante (
     id INT AUTO_INCREMENT,
     code INT NOT NULL, -- id at simo
