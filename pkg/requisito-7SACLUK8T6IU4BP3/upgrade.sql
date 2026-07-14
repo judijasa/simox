4bc921a2458132cb4829444c7674ee39300ff933
Juan Jaramillo <judijasa@gmail.com>
2026-07-14 10:09:01 -0500

    Change columns. pkg/requisito-7SACLUK8T6IU4BP3

--- pkg/requisito-7SACLUK8T6IU4BP3/upgrade.sql
+++ pkg/requisito-7SACLUK8T6IU4BP3/upgrade.sql
@@ -1,27 +1,14 @@
-/* To DROP a parent tbl, first you need to DROP its child tbls */
 CREATE OR REPLACE TABLE requisito (
     id INT AUTO_INCREMENT,
-    simo_id INT,
-    opec INT UNIQUE NOT NULL,
-    estudio_id INT,
-    experiencia_id INT,
-    otros_id INT,
-    alternativa_id INT,
+    code INT,
+    estudio TEXT,
+    experiencia TEXT,
+    otros TEXT,
+    alternativas JSON,
     equivalencias JSON,
 
-    PRIMARY KEY(id),
-    FOREIGN KEY fk_requisito_estudio_id(estudio_id)
-      REFERENCES estudio(id),
-    FOREIGN KEY fk_requisito_experiencia_id(experiencia_id)
-      REFERENCES experiencia(id),
-    FOREIGN KEY fk_requisito_otros_id(otros_id)
-      REFERENCES otros(id),
-    FOREIGN KEY fk_requisito_alternativa_id(alternativa_id)
-      REFERENCES alternativa(id)
+    PRIMARY KEY pk_requisito_id(id),
+    UNIQUE INDEX uk_requisito_code(code)
 );
 
 GRANT SELECT ON {{dbname}}.requisito TO 'public'@'{{servername}}';
-
--- We make this table instead of adding these cols directly
--- on job_offer because it allows many rows associated
--- to the same opec
