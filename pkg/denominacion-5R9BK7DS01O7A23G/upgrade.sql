e0e79509716de9c005b589696b7d7bbfec088ae9
Juan Jaramillo <judijasa@gmail.com>
2026-07-14 10:19:48 -0500

    Refactor pkg/denominacion-5R9BK7DS01O7A23G/upgrade.sql

--- pkg/denominacion-5R9BK7DS01O7A23G/upgrade.sql
+++ pkg/denominacion-5R9BK7DS01O7A23G/upgrade.sql
@@ -1,13 +1,8 @@
-/*
-To DROP a parent tbl, first you need to DROP its child tbls,
-that is why we make sure job_offer is dropped before
-replacing the denominacion table.
-*/
 CREATE OR REPLACE TABLE denominacion (
     id INT AUTO_INCREMENT,
     code INT NOT NULL,  -- id at simo
     nivel VARCHAR(100),
-    nombre VARCHAR(10000) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
 
     PRIMARY KEY(id),
     UNIQUE KEY uk_denominacion_code (code)
