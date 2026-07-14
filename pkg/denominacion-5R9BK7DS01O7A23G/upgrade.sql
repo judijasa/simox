98c61e381f86cde5697434d7f67e8d3eb037fed6
Juan Jaramillo <judijasa@gmail.com>
2026-07-14 11:50:06 -0500

    Change column: nivel_code->nivel_id. pkg/denominacion-5R9BK7DS01O7A23G

--- pkg/denominacion-5R9BK7DS01O7A23G/upgrade.sql
+++ pkg/denominacion-5R9BK7DS01O7A23G/upgrade.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE TABLE denominacion (
     id INT AUTO_INCREMENT,
     code INT NOT NULL,  -- id at simo
-    nivel_code TINYINT,
+    nivel_id TINYINT,
     nombre VARCHAR(100) NOT NULL,
 
     PRIMARY KEY(id),
     UNIQUE KEY uk_denominacion_code (code),
-    FOREIGN KEY fk_denominacion_nivel_code(nivel_code)
-        REFERENCES nivel(code)
+    FOREIGN KEY fk_denominacion_nivel_id(nivel_id)
+        REFERENCES nivel(id)
 );
 
 GRANT SELECT ON {{dbname}}.denominacion TO 'public'@'{{servername}}';
