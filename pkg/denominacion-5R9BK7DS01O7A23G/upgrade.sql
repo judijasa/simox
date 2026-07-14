d1796a550872793c265d633552ec42b176be2699
Juan Jaramillo <judijasa@gmail.com>
2026-07-14 10:32:23 -0500

    Change column nivel->nivel_code. pkg/denominacion-5R9BK7DS01O7A23G

--- pkg/denominacion-5R9BK7DS01O7A23G/upgrade.sql
+++ pkg/denominacion-5R9BK7DS01O7A23G/upgrade.sql
@@ -1,11 +1,12 @@
 CREATE OR REPLACE TABLE denominacion (
     id INT AUTO_INCREMENT,
     code INT NOT NULL,  -- id at simo
-    nivel VARCHAR(100),
+    nivel_code TINYINT,
     nombre VARCHAR(100) NOT NULL,
 
     PRIMARY KEY(id),
-    UNIQUE KEY uk_denominacion_code (code)
+    UNIQUE KEY uk_denominacion_code (code),
+    INDEX ix_denominacion_nivel_code(nivel_code)
 );
 
 GRANT SELECT ON {{dbname}}.denominacion TO 'public'@'{{servername}}';
