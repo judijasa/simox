d1893c0a43e6a2bfb152d59143a1530ee3434490
Juan Jaramillo <judijasa@gmail.com>
2026-07-14 19:46:02 -0500

    Add phprun with Agent/CronJob attributes and reflection-based enforcement

---
+++ bin/phprun.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+$arg = $argv[1] ?? null;
+
+if (!$arg || !str_contains($arg, ':')) {
+    fwrite(STDERR, "Usage: phprun.php <script.php:agent(args)>\n");
+    exit(1);
+}
+
+[$script, $func_call] = explode(':', $arg, 2);
+$func      = substr($func_call, 0, strpos($func_call, '('));
+$func_args = rtrim(substr($func_call, strpos($func_call, '(') + 1), ')');
+
+$repo_root = getenv('SIMO_REPO_PATH');
+if (!$repo_root || getcwd() !== $repo_root) {
+    fwrite(STDERR, "Error: must be run from the repository's root directory.\n");
+    exit(1);
+}
+
+require __DIR__ . '/../src/utils/attributes.php';
+require $script;
+
+$rf = new ReflectionFunction($func);
+if (empty($rf->getAttributes(\Utils\Agent::class))) {
+    fwrite(STDERR, "Error: '$func' in '$script' is not an #[Agent].\n");
+    exit(1);
+}
+
+printf('%s - Starting %s' . PHP_EOL, date('Y-m-d H:i:s'), $arg);
+eval("$func($func_args);");
+printf('%s - Finished %s' . PHP_EOL, date('Y-m-d H:i:s'), $arg);
