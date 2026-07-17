4f8f8035533093e772fc16018fdcdfb86271aaaf
Juan Jaramillo <judijasa@gmail.com>
2026-07-17 11:33:39 -0500

    Rename src/utils/logger.php -> src/utils/Logger.php. Remove agent.php and create Agent.php and CronJob.php

---
+++ src/utils/Agent.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils;
+
+#[Attribute]
+class Agent {}
--- src/utils/CronJob.php
+++ src/utils/CronJob.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace Utils;
 
 #[Attribute]
-class Agent {}
-
-#[Attribute]
 class CronJob
 {
     public function __construct(public readonly string $schedule) {}
