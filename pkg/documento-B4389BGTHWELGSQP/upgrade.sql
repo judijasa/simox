bc315cb9018ffde0336cad285e020e9c0ee7dbc6
Juan Jaramillo <judijasa@gmail.com>
2026-07-18 18:04:46 -0500

    Rename documento column: codigo -> code

--- pkg/documento-B4389BGTHWELGSQP/upgrade.sql
+++ pkg/documento-B4389BGTHWELGSQP/upgrade.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE TABLE documento (
     id INT AUTO_INCREMENT,
-    codigo INT,  -- simo id
+    code INT,  -- simo id
     ruta_archivo VARCHAR(100),
     content_type VARCHAR(100),
     version VARCHAR(100),
@@ -10,7 +10,7 @@ CREATE OR REPLACE TABLE documento (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY pk_documento_id(id),
-    UNIQUE INDEX uk_documento_codigo(codigo)
+    UNIQUE INDEX uk_documento_code(code)
 );
 
 GRANT SELECT ON {{dbname}}.documento TO 'public'@'{{servername}}';
