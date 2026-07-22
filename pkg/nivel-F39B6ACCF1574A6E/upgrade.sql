6144e637458d288f2733e46590e1722ba5387e6d
Juan Jaramillo <judijasa@gmail.com>
2026-07-21 19:11:26 -0500

    (Re)create `nivel` table. pkg/nivel-F39B6ACCF1574A6E

---
+++ pkg/nivel-F39B6ACCF1574A6E/upgrade.sql
@@ -0,0 +1,11 @@
+-- data taken from empleo.denominacion.nivel
+CREATE OR REPLACE TABLE nivel (
+    id SMALLINT UNSIGNED AUTO_INCREMENT,
+    code SMALLINT UNSIGNED,
+    nombre VARCHAR(100) UNIQUE NOT NULL,
+
+    PRIMARY KEY pk_nivel_id(id),
+    UNIQUE KEY uk_nivel_nombre(nombre)
+);
+
+GRANT SELECT ON {{dbname}}.nivel TO 'public'@'{{servername}}';
