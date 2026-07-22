11d3fb3e9598d9779a92f1f3980eab8ba394ea83
Juan Jaramillo <judijasa@gmail.com>
2026-07-21 19:02:25 -0500

    Add AUTO_INCREMENT to PK in estudio tbl.

--- pkg/estudio-LZ4GPR10TNN5SO2L/upgrade.sql
+++ pkg/estudio-LZ4GPR10TNN5SO2L/upgrade.sql
@@ -1,10 +1,10 @@
 /* To DROP a parent tbl, first you need to DROP its child tbls */
 CREATE OR REPLACE TABLE estudio (
-    id INT,
+    id INT AUTO_INCREMENT,
     descripcion TEXT,
     -- TODO: add cols that are synthesis of descr
 
-    PRIMARY KEY(id) -- TODO: Complete this schema
+    PRIMARY KEY(id)
 );
 
 GRANT SELECT ON {{dbname}}.estudio TO 'public'@'{{servername}}';
