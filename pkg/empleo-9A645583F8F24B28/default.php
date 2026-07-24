82673da877c1fd3cb0d8c75b0715cc5070119a71
Juan Jaramillo <judijasa@gmail.com>
2026-07-23 21:51:50 -0500

    Add missing dependencies to pkg/empleo-9A645583F8F24B28/default.php

--- pkg/empleo-9A645583F8F24B28/default.php
+++ pkg/empleo-9A645583F8F24B28/default.php
@@ -2,9 +2,12 @@
 $dependencies = array(
     'vacante-WZ0T87MRR815E49Y',
     'requisito-7SACLUK8T6IU4BP3',
+    'entidad-67REHGMCNAEUY2R1',
     'empleo_snapshot-00B5FEB6B8B84570',
+    'documento-B4389BGTHWELGSQP',
     'departamento-BD672733D7AA455F',
-    'denominacion-5R9BK7DS01O7A23G'
+    'denominacion-5R9BK7DS01O7A23G',
+    'convocatoria-CP5LH0NIV4WY9J5B'
 );
 ?>
 
