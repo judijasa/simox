efd0b567ea0a4bf12256b3cf2d79b5631a94eed7
Juan Jaramillo <judijasa@gmail.com>
2026-07-23 21:59:38 -0500

    Add necessary (and remove unnecessary) dependencies. pkg/vw_empleo-DAF75B74DD6A469D/default.php

--- pkg/vw_empleo-DAF75B74DD6A469D/default.php
+++ pkg/vw_empleo-DAF75B74DD6A469D/default.php
@@ -1,6 +1,12 @@
 <?php
 $dependencies = array(
-    'empleo-9A645583F8F24B28'
+    'vacante-WZ0T87MRR815E49Y',
+    'requisito-7SACLUK8T6IU4BP3',
+    'funcion-12ZLRQNT99GB3W8Z',
+    'empleo_vacante-AGC76TYMHR1LD9PT',
+    'empleo_requisito-9KS537SIXRO89TUK',
+    'empleo_funcion-S9N4FY21N92CQDTS',
+    'empleo-9A645583F8F24B28',
 );
 ?>
 
